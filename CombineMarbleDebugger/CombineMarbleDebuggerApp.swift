6a4afe67e70ef72eeb58c4b438d66ae7ea13c438
vlad.mataringa@gmail.com <Vlad Mataringa>
2023-05-24 21:16:51 +0300

    Initial commit with project

---
+++ CombineMarbleDebugger/CombineMarbleDebuggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineMarbleDebuggerApp.swift
+//  CombineMarbleDebugger
+//
+//  Created by Mataranga, Vlad on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineMarbleDebuggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
